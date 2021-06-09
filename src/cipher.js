89fe74dc0fbbab39b8eeff5dfad2e0cd53303085
jjullimayanne <julli.souza@ufob.edu.br>
2021-06-09 16:32:55 -0300

    DVDFV

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,49 @@
-const cipher = {
-  // ...
-};
 
-export default cipher;
+const cipher = {
+
+  //codificar
+  encode: function (offset, string) {
+    let txt = "";
+    for (let i = 0; i < string.length; i++) {
+
+      //todas as letras asc ficam entre 65 e 90
+
+      //codigo da letra charCodeat
+      //(codigo da letra  + descoc)  % tamanho do alfabeto ~volta (mod)
+      let valor = string.charCodeAt(i);
+      //todas as letras asc ficam entre 65 e 90
+      if (valor >= 65 && valor <= 90) {
+
+        //alfabeto + 0 =26
+        txt += String.fromCharCode((valor - 65 + parseInt(offset)) % 26 + 65);
+
+        //especial 
+      } else if (valor >= 97 && valor <= 122) {
+        txt += String.fromCharCode((valor - 97 + parseInt(offset)) % 26 + 97);
+      } else {
+        txt += string[i];
+      }
+    }
+    return txt;
+  },
+
+
+
+
+
+  //decodificar
+  decode: function (offset, string) {
+    let txt = "";
+    for (let i = 0; i < string.length; i++) {
+      let valor = string.charCodeAt(i);
+      if (valor >= 65 && valor <= 90) {
+        txt += String.fromCharCode((valor - 90 + parseInt(offset)) % 26 + 90);
+      } else if (valor >= 97 && valor <= 122) {
+        txt += String.fromCharCode((valor - 122 + parseInt(offset)) % 26 + 122);
+      } else {
+        txt += string[i];
+      }
+    }
+    return txt;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const deslocamento = document.getElementById("chave");
+const texto = document.getElementById("texto");
+const textoCifrado = document.getElementById("cifrado");
+const click = document.getElementById("click");
+const clickDec = document.getElementById("descifrar");
+const primeMsg = document.getElementById("hello");
+
+function cifrado() {
+    primeMsg.value = "Quer uma nova senha?";
+
+  /*document.getElementById("saludo").innerHTML = mensajeDefault.value;/*/
+  document.getElementById("hello").innerHTML = primeMsg.value;
+
+  const textoIngresado = texto.value;
+  const valorDeslocamento = parseInt(deslocamento.value);
+  if (valorDeslocamento> 0) {           
+    const textoCifrado = document.getElementById("cifrado");
+    textoCifrado.value = cipher.encode(valorDeslocamento, textoIngresado);
+    document.getElementById("cifrado").innerHTML = textoCifrado.value;
+  }
+  else {
+    textoCifrado.value = cipher.decode(valorDeslocamento, textoIngresado);
+    document.getElementById("cifrado").innerHTML = textoCifrado.value;
+  }
+}
+
+function decifrar() {
+  const textoADecifrar = document.getElementById("texto");
+  const textoDecifrado = textoADecifrar.value;
+  const valorDesplazamiento = - parseInt(deslocamento .value); 
+  textoADecifrar.value = cipher.decode(valorDesplazamiento, textoDecifrado);
+  document.getElementById("cifrado").innerHTML = textoADecifrar.value;
+ /* mensajeDefault.value = cipher.decode(-3, mensajeDefault.value);
+  document.getElementById("cifrado").innerHTML = mensajeDefault.value;*/
+}
+
+function mostrar() {
+  const textoIngresado = texto.value;
+  if (textoIngresado == '') {
+    alert('Coloque sua mensagem!');
+  }
+  else {
+    if (document.getElementById('burbuja').style.display == 'none') {
+      document.getElementById('burbuja').style.display = 'flex';
+    }
+    else {
+      document.getElementById('burbuja').style.display = 'flex';
+    }
+  }
+}
+
+
+click.addEventListener("click", mostrar);
+click.addEventListener("click", cifrado);
+
+clickDec.addEventListener("click", mostrar);
+clickDec.addEventListener("click", decifrar);
