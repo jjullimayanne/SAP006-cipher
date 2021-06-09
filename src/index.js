9671cbd24584fb2c4be0d415fe594c2bda865a43
jjullimayanne <julli.souza@ufob.edu.br>
2021-06-09 16:49:01 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -32,8 +32,6 @@ function decifrar() {
   const valorDesplazamiento = - parseInt(deslocamento .value); 
   textoADecifrar.value = cipher.decode(valorDesplazamiento, textoDecifrado);
   document.getElementById("cifrado").innerHTML = textoADecifrar.value;
-  mensajeDefault.value = cipher.decode(-3, mensajeDefault.value);
-  document.getElementById("cifrado").innerHTML = mensajeDefault.value;
 }
 
 function mostrar() {
